b9c51cd77699e2b9fbfd3ce6f5a1f80b89abefa8
Alejandro <alejandro@Alejandros-MacBook-Pro.local>
2022-12-05 10:23:05 -0600

    Add checkuser endpoint

--- main.py
+++ main.py
@@ -27,6 +27,52 @@ ports_enviados = [3001]
 
 # Crear funcion para mandar el mensaje
 
+async def send_message_main(i, url_send):
+    try:
+        ports_to_send_df = await send_message('http://localhost:3000/api/botprocess')
+        port = random.choice(ports_to_send_df['port'])
+        if (len(ports_enviados) < 2):
+            ports_enviados.append(port)
+        else:
+            if (port == ports_enviados[-1]):
+                port = random.choice(ports_to_send_df['port'])
+        ports_enviados.append(port)
+        params_random_response = requests.get(
+            'http://localhost:3000/api/sendparams').content
+        minValue = json.loads(params_random_response)[0]['minValue']
+        maxValue = json.loads(params_random_response)[0]['maxValue']
+        random_sync_sleep = random.randint(minValue, maxValue)
+        my_uuid = uuid.uuid4()
+        my_uuid = str(my_uuid)
+        try:
+            send_text = requests.get(
+                f'http://localhost:{port}{url_send}/{i[1]}/{i[0]}')
+            if (send_text.status_code != 201):
+                print(send_text)
+                print(
+                    f'Problema Grave con el envio status code: {send_text.status_code}')
+            else:
+                print(send_text)
+        except Exception as e:
+            print(e)
+            print(
+                'problema no se pudo mandar se va amandar el mensaje de otro puerto')
+            # await asyncio.sleep(15)
+            try:
+                ports_to_send_df = await send_message('http://localhost:3000/api/botprocess')
+                port = random.choice(ports_to_send_df['port'])
+                send_text = requests.get(
+                    f'http://localhost:{port}{url_send}/{i[1]}/{i[0]}')
+            except Exception as e:
+                print(e)
+        print(
+            f'Mensaje terminando esperando para el siguiente {random_sync_sleep} segundos')
+        # await asyncio.sleep(random_sync_sleep)
+        print('Mensaje enviado')
+    except Exception as e:
+        print(e)
+        pass
+
 
 async def send_message(url):
     bool_value = True
@@ -115,6 +161,15 @@ async def upload(file: UploadFile = File(...)):
             print(e)
             pass
 
+@app.post("/ckeckuser")
+async def upload(file: UploadFile = File(...)):
+    contents = file.file.read()
+    with open(file.filename, 'wb') as f:
+        f.write(contents)
+    os.rename(file.filename, 'assets/data.csv')
+    files = pd.read_csv('assets/data.csv')
+    for i in files.values:
+        await send_message_main(i, '/api/whatsapp/checkuser')
 
 @app.get("/")
 def main():
